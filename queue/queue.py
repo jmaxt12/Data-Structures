7b364e77def02020a7fb415c3287fe8dc992aec3
Jake Thomas <jmaxt12@gmail.com>
2019-06-10 14:18:27 -0600

    added notes to queue

--- queue/queue.py
+++ queue/queue.py
@@ -5,16 +5,16 @@ class Queue:
     # use to store queue elements?
     self.storage = []
 
-  def enqueue(self, item):
+  def enqueue(self, item): #should add an item to the back of the queue.
     self.storage.insert(0, item)
     self.size += 1
   
-  def dequeue(self):
+  def dequeue(self): #should remove and return an item from the front of the queue.
     if self.size == 0:
       return None
     else:
       self.size -= 1
       return self.storage.pop()
 
-  def len(self):
+  def len(self): #returns the number of items in the queue.
     return self.size
